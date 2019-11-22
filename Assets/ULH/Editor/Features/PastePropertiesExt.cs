4ec7f5873221c14ef62c31946bf9535b513a2cb6
Park June Chul <pjc0247@naver.com>
2019-11-22 12:53:12 +0900

    Prototype of PasteProperties

---
+++ Assets/ULH/Editor/Features/PastePropertiesExt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEditor;
+
+namespace LittleHelper
+{
+    public class PastePropertiesExt
+    {
+        private static UnityEngine.Object srcObject;
+
+        [MenuItem("CONTEXT/Object/Copy Properties", priority = 5000)]
+        public static void CopyProperties(MenuCommand command)
+        {
+            srcObject = command.context;
+        }
+        [MenuItem("CONTEXT/Object/Paste Properties", priority = 5000)]
+        public static void PasteProperties(MenuCommand command)
+        {
+            if (srcObject == null) return;
+            if (command.context == srcObject) return;
+
+            var sobj = new SerializedObject(srcObject);
+            var dstObj = new SerializedObject(command.context);
+
+            var it = sobj.GetIterator();
+            while (it.Next(true))
+            {
+                dstObj.CopyFromSerializedProperty(it);
+            }
+            dstObj.ApplyModifiedProperties();
+        }
+    }
+}
